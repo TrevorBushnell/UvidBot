12f1aea7aa71937451f17d0c0740a9f858adb918
TrevorBushnell <tbushnell11@gmail.com>
2026-04-30 20:53:09 -0700

    better type inference

--- main.ts
+++ main.ts
@@ -49,7 +49,7 @@ client.login(process.env.DISCORD_TOKEN);
 client.on(Events.InteractionCreate, async (interaction: Interaction) => {
     console.log(interaction);
     if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
+    const command: SlashCommand | undefined = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found.`);
